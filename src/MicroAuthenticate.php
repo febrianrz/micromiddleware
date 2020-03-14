969f312ab841266afcfeb301fe9732ce1c40d7d0
febrian reza <febrianrz@gmail.com>
2020-03-14 16:58:40 +0700

    initial commit

---
+++ src/MicroAuthenticate.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Febrianrz\Micromidlleware;
+
+use Closure;
+use GuzzleHttp\Client;
+
+class MicroAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = $this->getUser($request);
+        if($user){
+            $request->merge([
+                'user'  => $user
+            ]);
+            return $next($request);
+        } 
+        return response()->json([
+            'message'   => 'Cannot access this resource or unauthenticated'
+        ]);
+        
+    }
+
+    private function getUser($request){
+        try {
+            $auth_url = config('micro')['url']['auth'];
+            $client = new Client([
+                'base_uri'  => $auth_url,
+                'timeout'   => 2.0
+            ]);
+            $response = $client->request('GET', '/api/v1/profile', [
+                'headers' => [
+                    'Authorization' => $request->header('Authorization'),
+                    'Accept'     => 'application/json',
+                ]
+            ]);
+            if($response->getStatusCode() == 200){
+                return json_decode($response->getBody()->getContents());
+            } 
+            return null;
+        } catch(\Exception $e){
+            return abort(500,$e->getMessage());
+        }
+    }
+}
